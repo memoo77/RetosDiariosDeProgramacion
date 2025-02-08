9de0cad6d8adf6f9ccce0265bc375ebc7c680b46
memoo77 <Guille@MacBook-Air-de-Luci.local>
2025-02-07 23:46:40 -0300

    deepClone80%

--- retos/desafios/Avanzados/deepClone-especial.js
+++ retos/desafios/Avanzados/deepClone-especial.js
@@ -36,27 +36,31 @@ Propósito del desafío:
 Aprender a manipular objetos en profundidad, entender cómo funcionan las referencias en JavaScript y mejorar el conocimiento sobre programación recursiva.
 */
 
-let copy = {}
+
 const deepClone = ( objToClone ) => {
 
+  let copyObj = Array.isArray(objToClone) ? [] : {};
   // valida si es un primitivo, si lo es lo devuelve de forma directa, si no
   // mira si es un array o un objeto
-  if (typeof(objToClone) !== 'object' || objToClone === null) {
-    return objToClone;
-  }
 
-  const copyObj = Array.isArray(objToClone) ? [] : {};
+  if (typeof(objToClone) === 'function') {
+    return objToClone;
+    //en este caso se asigna directamente al objToclone, para que salga del deepClone
+    //y poble el copy, de esta manera referencia la función y todo opera tal cual.
+  }
+  if (typeof(objToClone) !== 'object' || objToClone === null) {
+    return copyObj;
+  }
 
   //en este ciclo for in procesa cada propiedad del objeto
   //se itera y realiza una recursividad asi mismo
   //buscando encontrar el valor primitivo para poder asignarlo a la key correspondiente
   for (const key in objToClone) {
- 
-    copyObj[key] = deepClone(objToClone[key])
-    
-    
+    if (objToClone.hasOwnProperty(key)){
+      copyObj[key] = deepClone(objToClone[key])
+    }
   }
-  return copy;
+  return copyObj;
 }
 
 const original = {  //objeto primer nivel
@@ -70,16 +74,21 @@ const original = {  //objeto primer nivel
     }
   };
   
-  copy = deepClone(original);
-  console.table({copy})
+  const copy = deepClone(original);
+  console.log({copy})
   copy.details.age = 10;
   copy.details.data.push(4);
   
-  console.log(original.details.age); // 5
-  console.log(original.details.data); // [1, 2, 3]
-  console.log(copy.details.age); // 10
+  console.log(`original.details.age: ${original.details.age}`); // 5
+  console.log(`${original.details.data}`); // [1, 2, 3]
+  console.log(`copy.details.age: ${copy.details.age}`); // 10
   console.log(copy.details.data); // [1, 2, 3, 4]
+
+  console.log(original.method()); // "Hola"
   console.log(copy.method()); // "Hola"
+
+  copy.method = () => {return 'Hola desde copy'};
+  console.log(copy.method())
 
 
 
